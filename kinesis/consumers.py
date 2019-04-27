2a0e293bf73bc9bbd43669ca3274c6062487f2eb
ashimagg <ashim.agg93@gmail.com>
2019-04-26 23:21:01 -0400

    consumer code

--- kinesis/consumers.py
+++ kinesis/consumers.py
@@ -1,5 +1,7 @@
 from boto.kinesis.exceptions import ProvisionedThroughputExceededException
 import datetime
+import boto3
+import time
 
 class KinesisConsumer:
     """Generic Consumer for Amazon Kinesis Streams"""
@@ -18,6 +20,7 @@ class KinesisConsumer:
 
     @staticmethod
     def iter_records(records):
+        print ("Num of records - " + str(len(records)))
         for record in records:
             part_key = record['PartitionKey']
             data = record['Data']
@@ -25,8 +28,8 @@ class KinesisConsumer:
 
     def run(self):
         """poll stream for new records and pass them to process_records method"""
-        response = kinesis.get_shard_iterator(self.stream_name,
-            self.shard_id, self.iterator_type)
+        response = kinesis.get_shard_iterator(StreamName = self.stream_name,
+            ShardId = self.shard_id, ShardIteratorType = self.iterator_type)
 
         next_iterator = response['ShardIterator']
 
@@ -35,10 +38,10 @@ class KinesisConsumer:
 
         while finish > datetime.datetime.now():
             try:
-                response = kinesis.get_records(next_iterator, limit=25)
+                response = kinesis.get_records(ShardIterator = next_iterator, Limit=25)
 
                 records = response['Records']
-
+                print ("Records - " + str(len(records)))
                 if records:
                     self.process_records(records)
 
@@ -46,3 +49,16 @@ class KinesisConsumer:
                 time.sleep(self.sleep_interval)
             except ProvisionedThroughputExceededException as ptee:
                 time.sleep(1)
+    
+    def process_records(self, records):
+        """print the partion key and data of each incoming record"""
+        for part_key, data in self.iter_records(records):
+            print(part_key, ":", data)
+
+session = boto3.Session(profile_name='ash')
+kinesis = session.client('kinesis')
+shard_id = 'shardId-000000000010'
+iterator_type = 'LATEST'
+stream_name = "mta-data"
+worker = KinesisConsumer(stream_name, shard_id, iterator_type, worker_time=10)
+worker.run()
