a1979eca763b188fc381cf1648b48151c48e19ab
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-04-30 01:20:49 -0400

    kafka consumer

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,5 +1,24 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('my_favorite_topic')
 
-for msg in consumer:
-	print (msg)
+from properties import subwayLines
+import threading
+
+class Kafka(threading.Thread):
+	def __init__(self, stream_name, sleep_interval=None):
+		self.stream_name = stream_name
+		self.sleep_interval = sleep_interval
+		super().__init__()
+
+	def read_record(self):
+		consumer = KafkaConsumer(self.stream_name, bootstrap_servers='localhost:9092')
+		for msg in consumer:
+			print(self.stream_name, msg.value.decode())
+
+	def run(self):
+		try:
+			self.read_record()
+		except Exception as e:
+			print(e)
+
+for line in subwayLines:
+	Kafka(line, sleep_interval=0).start()
--- kafka/producer.py
+++ kafka/producer.py
@@ -9,11 +9,6 @@ import time
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 class KafkaProducer(threading.Thread):
-	"""Producer class for AWS Kinesis streams
-
-	This class will emit records with the IP addresses as partition key and
-	the emission timestamps as data"""
-
 	def __init__(self, stream_name, sleep_interval=None):
 		self.stream_name = stream_name
 		self.sleep_interval = sleep_interval
@@ -23,16 +18,11 @@ class KafkaProducer(threading.Thread):
 		producer.send(self.stream_name, data.encode())
 		
 	def read_file(self):
-		"""put a record at regular intervals"""
-		# while True:
-		# 	self.put_record()
-		# 	time.sleep(self.sleep_interval)		
 		with open('../data/processed_data/'+self.stream_name+'.csv') as csvfile:
 			readCSV = csv.reader(csvfile, delimiter='\n')
 			for index,row in enumerate(readCSV):
 				if index>0:
 					self.put_record(row[0])
-					# print(row[0])
 					time.sleep(self.sleep_interval)
 
 	def run(self):
@@ -42,6 +32,5 @@ class KafkaProducer(threading.Thread):
 			print(e)
 
 
-
 for line in subwayLines:
-	KafkaProducer(line, sleep_interval=1).start()
+	KafkaProducer(line, sleep_interval=0.5).start()
